1adb39956b3f0e91a056b82e74667e603ae29609
Matt Jarvis <mattj23@gmail.com>
2020-12-02 00:01:45 -0500

    simple logger

--- MySqlBackupAgent/Services/BackupTargetService.cs
+++ MySqlBackupAgent/Services/BackupTargetService.cs
@@ -4,6 +4,7 @@ using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using MySqlBackupAgent.Models;
 
 namespace MySqlBackupAgent.Services
@@ -12,19 +13,24 @@ namespace MySqlBackupAgent.Services
     {
         private readonly ConcurrentDictionary<string, DbBackupTarget> _targets;
         private readonly IServiceScopeFactory _scopeFactory;
+        private readonly ILogger _logger;
         
-        public BackupTargetService(IConfiguration section, IServiceScopeFactory scopeFactory)
+        public BackupTargetService(IConfiguration section, IServiceScopeFactory scopeFactory, ILogger logger)
         {
             _scopeFactory = scopeFactory;
+            _logger = logger;
             _targets = new ConcurrentDictionary<string, DbBackupTarget>();
+            
 
             using var scope = _scopeFactory.CreateScope();
             var environment = scope.ServiceProvider.GetService<IWebHostEnvironment>();
             var workingPath = Path.Combine(environment.WebRootPath, "scratch_dir");
             
+            _logger.Log(LogLevel.Information, "Creating backup targets");
             foreach (var child in section.GetChildren())
             {
                 var target = new DbBackupTarget(child, workingPath, scopeFactory);
+                _logger.Log(LogLevel.Information, "Created target {0}", target.Name);
                 _targets[target.Name] = target;
                 _targets[target.Name].ScheduleNext();
             }
@@ -38,7 +44,9 @@ namespace MySqlBackupAgent.Services
     {
         public static void AddBackupTargets(this IServiceCollection services, IConfiguration config)
         {
-            services.AddSingleton(x => new BackupTargetService(config, x.GetService<IServiceScopeFactory>()));
+            services.AddSingleton(x => new BackupTargetService(config, 
+                x.GetService<IServiceScopeFactory>(), 
+                x.GetService<ILogger<BackupTargetService>>()));
         }
     }
 }
